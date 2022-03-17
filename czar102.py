7c3b88ba100e5ad2078ec6be4d524b4158a41eed
Czar102 <79329479+Czar102@users.noreply.github.com>
2022-03-17 15:20:39 +0100

    delete bloat

--- czar102.py
+++ czar102.py
@@ -5,16 +5,24 @@ from utils.eth import get_contract
 
 nullAddress = "0x0000000000000000000000000000000000000000"
 
-w3 = Web3(Web3.HTTPProvider('https://arb1.arbitrum.io/rpc')) # https://arb1.arbitrum.io/rpc
+w3 = Web3(Web3.HTTPProvider('https://arb1.arbitrum.io/rpc')) # https://arb1.arbitrum.io/rpc  https://rinkeby.arbitrum.io/rpc
 print(w3.isConnected())
-contract = "0x35Bcf3c30594191d53231E4FF333E8A770453e40"
+contract = "0x35Bcf3c30594191d53231E4FF333E8A770453e40" # 0xe42229d764F673EB3FB8B9a56016C2a4DA45ffd7
 BondingManager = get_contract(w3, contract, bonding_manager_contract_abi)
 
 
 def transferBondOwnership(sender, senderPrivate, recipient):
-	amount = BondingManager.functions.getDelegator(sender).call()[3]
+	ret = BondingManager.functions.getDelegator(sender).call()
+	print(ret)
+	amount = ret[0]
 
-	tx = BondingManager.functions.transferBond(recipient, amount, nullAddress, nullAddress, nullAddress, nullAddress).buildTransaction()
+	tx = BondingManager.functions.transferBond(recipient, amount, nullAddress, nullAddress, nullAddress, nullAddress).buildTransaction(
+		{
+			"from": sender,
+			"gasPrice": 1000000000,
+			"nonce": w3.eth.get_transaction_count(sender)
+		}
+	)
 
 	signedTx = w3.eth.account.sign_transaction(tx, senderPrivate)
 
@@ -26,43 +34,8 @@ def transferBondOwnership(sender, senderPrivate, recipient):
 	print(receipt)
 
 
+sender = input("Pass sender account: ")
+senderPrivate = input("Input private key: ")
+recipient = "0xd541B4316623Ed0Af28d029Bd447fD4c45472d68"
 
-# 0. for testing purposes only!
-
-# 1. send funds from the funder to the first account
-# needs approx 0.0032 eth
-
-
-def getTxOverWith(txDict, senderPrivate):
-	signed_txn = w3.eth.account.sign_transaction(txDict, senderPrivate)
-
-	transactionHash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
-
-	receipt = w3.eth.wait_for_transaction_receipt(transactionHash)
-
-	print("Pure tx:")
-	print(receipt)
-
-
-txInfo = [
-	{
-		"from": "0x08f10D03A0CF7a9eADdc7EacD4cf135a07A0feff",
-		"fromPrivate": "", # Paste private for 0x08f10D03A0CF7a9eADdc7EacD4cf135a07A0feff here
-		"data": "062e98b800000000000000000000000052cAC9F2df3068107c95DB3Acf3f6B1256d75E4500000000000000000000000000000000000000000000000384e3368bea0d64aa0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
-	},
-	{
-		"from": "0xb1D0Bac0abBdfa8D7Ca6fA0973cfE84F79262967",
-		"fromPrivate": "", # Paste private for 0xb1D0Bac0abBdfa8D7Ca6fA0973cfE84F79262967 here
-		"data": "0x062e98b800000000000000000000000052cac9f2df3068107c95db3acf3f6b1256d75e45000000000000000000000000000000000000000000000008724232c4e68c01b40000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
-	},
-	{
-		"from": "0x1D2A89755cA40B1226b4D7A52E8aDDa610D08d46",
-		"fromPrivate": "", # Paste private for 0x1D2A89755cA40B1226b4D7A52E8aDDa610D08d46 here
-		"data": "0x062e98b800000000000000000000000052cac9f2df3068107c95db3acf3f6b1256d75e45000000000000000000000000000000000000000000000008c2c9ad2738df559c0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
-	},
-]
-
-
-# 2. wait until funds are rebonded
-
-# 2. rebond funds on 0x08f10d03a0cf7a9eaddc7eacd4cf135a07a0feff
+transferBondOwnership(sender, senderPrivate, recipient)
