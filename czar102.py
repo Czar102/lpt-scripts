f4fab5e1c5974f07f1f896ab9c35747547b37e91
Czar102 <79329479+Czar102@users.noreply.github.com>
2022-10-30 23:16:32 +0100

    tweaks

--- czar102.py
+++ czar102.py
@@ -1,5 +1,4 @@
 from web3 import Web3
-import binascii
 from utils.livepeer.abi.bonding_manager import bonding_manager_contract_abi
 from utils.eth import get_contract
 
@@ -9,7 +8,6 @@ w3 = Web3(Web3.HTTPProvider('https://arb1.arbitrum.io/rpc')) # https://arb1.arbi
 print(w3.isConnected())
 contract = "0x35Bcf3c30594191d53231E4FF333E8A770453e40" # 0xe42229d764F673EB3FB8B9a56016C2a4DA45ffd7
 BondingManager = get_contract(w3, contract, bonding_manager_contract_abi)
-
 
 def transferBondOwnership(sender, senderPrivate, recipient):
 	ret = BondingManager.functions.getDelegator(sender).call()
@@ -33,9 +31,8 @@ def transferBondOwnership(sender, senderPrivate, recipient):
 	print("Bond ownership transfer:")
 	print(receipt)
 
-
 sender = input("Pass sender account: ")
 senderPrivate = input("Input private key: ")
-recipient = "0xd541B4316623Ed0Af28d029Bd447fD4c45472d68"
+recipient = ""
 
 transferBondOwnership(sender, senderPrivate, recipient)
